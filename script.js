5fded8ede2bd6a76994908c05b893d95a8402a14
George Schedvin <george.schedvin@gmail.com>
2023-02-01 13:47:40 +0100

    Final update

--- script.js
+++ script.js
@@ -1,138 +1,133 @@
 const BASE_URL = ('https://jsonplaceholder.typicode.com/todos');
-// const BASE_URL = ('https://jsonplaceholder.typicode.com/users/1/todos')
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const list = document.querySelector("#tasks");
 const output = document.querySelector('#output');
-const modalB = document.querySelector('#modal')
+const modal = document.querySelector('#modal')
 const closeModal = document.querySelector('#closeModal');
-modalB.classList.add('modalhide') 
+modal.classList.add('modalhide') 
 let todos = []
 
-//  Hämtar ärend
 fetch(BASE_URL + '?_limit=7') // '?_limit=7' limit är 7
-.then(res => {
-    return res.json();
+    // fetch(BASE_URL)
+    .then(res => {
+        return res.json();
 })
-.then(data =>{
-    // console.log(data);
-    todos = data
-    todos.forEach(todo => {
-        byggEttElement(todo)
+    .then(data =>{
+        todos = data
+        todos.forEach(todo => {
+            taskBuilder(todo)
     });
 }) 
 .catch(error => console.log(error))
 
-// Hindrar omladdningen
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-        const textVärdet = input.value.trim();
+        const inputValue = input.value.trim();
             const error = document.querySelector(".error");
                 error.textContent = "";
-//  Post 
 fetch(BASE_URL, {
     method: 'POST',
     body: JSON.stringify({
-        title: textVärdet,
+        title: inputValue,
         completed: false,
       }),
-      headers: {
+    headers: {
         'Content-type': 'application/json; charset=UTF-8',
-      },
-}) // om det inte är något textvärde så kommer felmeddelande. 
+    },
+}) 
     .then((response) => response.json())
-        .then((data) => {
-            if (textVärdet === '') {
-                    error.textContent = "Fyll i något du behöver göra";
+    .then((data) => {
+        if (inputValue === '') {
+            error.textContent = "*Fyll i något du behöver göra";
                 return;
-            } else {
-            // console.log(textVärdet)
-            byggEttElement(data)
+        } else {
+            taskBuilder(data)
         }
     });
 });
-// fabrik
-const byggEttElement = (data) => {
-    const task = document.createElement('div');
-	    task.classList.add('task');
-    const taskChild = document.createElement('div');
-	    taskChild.classList.add('content');
-            task.appendChild(taskChild);
-    const inputValue = document.createElement('input');
-	    inputValue.classList.add('text');
-        inputValue.classList.remove('active')  
-	    inputValue.type = 'text';
-	    inputValue.value = data.title;
-	    inputValue.setAttribute('readonly', 'readonly');
-            taskChild.appendChild(inputValue);
-// Knappfabriken
-const kroppen = document.createElement('div');
-	kroppen.classList.add('actions');
+const taskBuilder = (data) => {
+const task = document.createElement('div');
+const taskChild = document.createElement('div');
+const inputValue = document.createElement('input');
+    task.classList.add('task');
+    taskChild.classList.add('content');
+    inputValue.classList.add('text');
+	inputValue.value = data.title;
+    inputValue.setAttribute('readonly', 'readonly');
+        task.appendChild(taskChild);
+        taskChild.appendChild(inputValue);
+const actionParent = document.createElement('div');
 const actionEdit = document.createElement('button');
-	actionEdit.classList.add('edit');
-	    actionEdit.innerText = 'Edit';
 const actionDelete = document.createElement('button');
-	actionDelete.classList.add('delete');
-	    actionDelete.innerText = 'Delete'; 
 const actionDone = document.createElement('button');
-if (data.completed) {
-        inputValue.classList.add('active');
-}
-	actionDone.classList.add('done');
-	    actionDone.innerText = 'Done'; 
-
-kroppen.appendChild(actionDone);
-kroppen.appendChild(actionEdit);
-kroppen.appendChild(actionDelete);
-task.appendChild(kroppen);
-list.prepend(task); // tvärtom mot append, början
-
+    actionParent.classList.add('actions');
+    actionDone.classList.add('done');
+    actionEdit.classList.add('edit');
+	actionDelete.classList.add('delete');
+       actionEdit.innerText = 'Edit';
+       actionDelete.innerText = 'Delete'; 
+            if (data.completed) {
+                inputValue.classList.add('active');
+                actionDone.innerText = 'Ångra';
+            } else {
+	            actionDone.innerText = 'Done'; 
+            }
+actionParent.appendChild(actionDone);
+actionParent.appendChild(actionEdit);
+actionParent.appendChild(actionDelete);
+task.appendChild(actionParent);
+list.prepend(task); // append =/= prepend
 input.value = '';
 
-// redigera eller spara
 actionEdit.addEventListener('click', (e) => {
 	if (actionEdit.innerText.toLowerCase() == "edit") {
 		actionEdit.innerText = "Save";
-		inputValue.removeAttribute("readonly");
-		inputValue.focus();
+		    inputValue.removeAttribute("readonly");
+		        inputValue.focus();
 	} else {
 		actionEdit.innerText = "Edit";
 		inputValue.setAttribute("readonly", "readonly");
 	}
 }); 
-// radera task (förälder)
-
 actionDelete.addEventListener('click', (e) => {  
     if (inputValue.classList.contains('active')) {
-        modalB.classList.add('modalhide') 
-    } else {
-        modalB.classList.remove('modalhide')
-        return  
+        modal.classList.add('modalhide') 
+    } else {    
+        modal.classList.remove('modalhide')
+            return  
 }  
-    // fetch funktion och raderar till länken 
-    fetch(BASE_URL + '/' + data.id, {
-        method: 'DELETE'
-    })
-    .then(res => {
-        if (res.status == 200){
+fetch(BASE_URL + '/' + data.id, {
+    method: 'DELETE'
+})
+.then(res => {
+    if (res.status == 200){
             todos = todos.filter(todo => todo.id !== data.id)
             list.removeChild(task);
         }
     })
 });
-
-// Markerar färdiga 
 actionDone.addEventListener("click", (e) => {
-    if (inputValue.classList.contains('active')) {
-        actionDone.innerText = 'done'; 
-        inputValue.classList.remove('active');
-    } else {
-        inputValue.classList.add('active');
-        actionDone.innerText = 'ångra'; 
-    }
+    fetch(BASE_URL + '/' + data.id, {
+        method: 'PATCH',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        body: JSON.stringify({completed:!data.completed})
+    })
+        .then(res => res.json())
+        .then(todo => {
+            data.completed = todo.completed
+            if (data.completed) {
+                actionDone.innerText = 'ångra'; 
+                inputValue.classList.add('active');
+            } else {
+                inputValue.classList.remove('active');
+                actionDone.innerText = 'done'; 
+            }
+        })
 });
-
 closeModal.addEventListener('click', (e) => {
-    modalB.classList.add('modalhide')
+    modal.classList.add('modalhide')
 })
 };
